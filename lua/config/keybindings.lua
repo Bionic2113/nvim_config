bf85d6e93428268087e48766f2a1016166748481
Bionic2113 <lol_smail@bk.ru>
2024-01-08 13:37:40 +0500

    update

--- lua/config/keybindings.lua
+++ lua/config/keybindings.lua
@@ -3,7 +3,6 @@ g.mapleader = " " -- Use Space, like key for alternative hotkeys
 -- LSP {{{
 nm("K", "<cmd>lua vim.lsp.buf.hover()<CR>") -- Hover object
 nm("ga", "<cmd>lua vim.lsp.buf.code_action()<CR>") -- Code actions
-nm("gR", "<cmd>lua vim.lsp.buf.rename()<CR>") -- Rename an object
 nm("gD", "<cmd>lua vim.lsp.buf.declaration()<cr>") -- Go to declaration
 nm(",r", "<cmd>lua vim.lsp.buf.rename()<CR>") -- Переименование переменной
 nm(",f", "<cmd>lua vim.lsp.buf.format()<CR>") -- Форматирование
